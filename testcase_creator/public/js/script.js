3fd722d0b1e3ab1bd0827ab0f34fdfd976b876ba
Dolly Palencia <dollypalencia.work@gmail.com>
2025-03-01 16:14:18 +0200

    - Updated directory - Updated html files - Updated css files - Added js file

---
+++ testcase_creator/public/js/script.js
@@ -0,0 +1,7 @@
+function openCreateProjWindow() {
+    document.getElementById("new-proj").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("new-proj").style.display = "none";
+}
